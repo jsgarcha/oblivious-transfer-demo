a85f88dbb7558fcc3ea84ac432d71c657c5eda5e
Jasminder Garcha <jasminder.garcha@gmail.com>
2025-05-18 16:59:01 -0400

    RSA comments updated.

--- RSA_module.py
+++ RSA_module.py
@@ -1,26 +1,26 @@
 
-#'Crypto' is more secure than native math 'number' or 'random' Python libs.
+#'Crypto' is more secure for this application than native math 'number' or 'random' Python libs
 from Crypto.Util.number import getPrime, inverse
 from Crypto.Random import get_random_bytes
  
 class RSA:
-    #Generate an N-bit (roughly) public and private key pair.
+    #Generate an N-bit (roughly) public and private key pair
     def __init__(self, bit_length):
         p = getPrime(bit_length//2)
         q = getPrime(bit_length//2)
-        n = p*q #Modulus. n = p*q, size N.
+        n = p*q #Modulus. n = p*q, size N
         phi = (p-1)*(q-1)
-        e = getPrime(bit_length, randfunc=get_random_bytes) #Public exponent e, relatively prime to (p-1)*(q-1).
+        e = getPrime(bit_length, randfunc=get_random_bytes) #Public exponent e, relatively prime to (p-1)*(q-1)
         d = inverse(e, phi)
 
         self.modulus = n
-        self.public_key = e #Public key = (Public exponent = e, modulus = n = p*q).
-        self.private_key = d #Private key = private exponent = d = inverse(e, phi).
+        self.public_key = e #Public key = (Public exponent = e, modulus = n = p*q)
+        self.private_key = d #Private key = private exponent = d = inverse(e, phi)
 
-    #encrypt(self, m) = m^e mod PQ = m.
+    #encrypt(message) = message^(e%n)
     def encrypt(self, message):
         return pow(message, self.public_key, self.modulus)
 
-    #decrypt(e) = e^d mod PQ = e^private key mod modulus.
+    #decrypt(encrypted_message) = encrypted_message^(d%n) 
     def decrypt(self, encrypted_message):
         return pow(encrypted_message, self.private_key, self.modulus)
