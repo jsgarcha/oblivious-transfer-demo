a7ebb8dcf8fbd8ee70e16d0d4e6d56be8abc01c1
Jasminder Garcha <jasminder.garcha@gmail.com>
2025-05-18 00:37:21 -0400

    Update Agent.

--- agent.py
+++ agent.py
@@ -6,8 +6,10 @@ from Crypto.Random import get_random_bytes
 app = FastAPI()
 
 rsa = RSA(256)
-I = list(range(10))  #Information items
-RN = [get_random_bytes(32) for _ in I ]
+I = list(range(10)) #Information items
+RN = [get_random_bytes(32) for _ in I] #Step 1: Agent sends random numbers RN[1],...,RN[n] to the inquirer.	
+
+#Assume inquirer wants to know the kth information unit I[k].	
 
 class Step2Input(BaseModel):
     step2_value: str  # Changed from int to str
@@ -26,9 +28,9 @@ def init():
 def process_step2(data: Step2Input):
     step2_val = int(data.step2_value)  # Convert back to int
     responses = []
-    for i in range(len(info_items)):
+    for i in range(len(I)):
         diff = (step2_val - RN[i]) % rsa.n
         decrypted = rsa.decrypt(diff)
-        response = decrypted + info_items[i]
+        response = decrypted + I[i]
         responses.append(response)
     return {"responses": responses}
