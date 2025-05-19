9f388ab42f9659cb12d31eb2bd612e071e486630
Jasminder Garcha <jasminder.garcha@gmail.com>
2025-05-19 14:28:18 -0400

    Update RSA & step 1.

--- inquirer.py
+++ inquirer.py
@@ -1,7 +1,10 @@
 #Inquirer (receiver/client)
 import streamlit as st
+import pandas as pd
 import requests
 from Crypto.Random import get_random_bytes
+
+from RSA_module import RSA
 
 st.set_page_config("🔐 Inquirer - RSA based 1-out-of-n Oblivious Transfer Simulator", layout="wide")
 st.title("🔐 Inquirer - RSA based 1-out-of-n Oblivious Transfer Simulator")
@@ -60,36 +63,13 @@ if st.session_state.step0 and not st.session_state.step1:
             if response.status_code == 200:
                 step1_data = response.json()
                 st.session_state.RN = step1_data["RN"]
+
                 st.info("Received random numbers (RN[0],...,RN[n-1]) from Agent.")
-                st.write("RN[]:")
-                st.write(st.session_state.RN)
+
+                st.subheader("Agent's random numbers (RN[]):")
+                st.dataframe(pd.DataFrame(st.session_state.RN, columns=['Agent Random Number (RN[i])']))
 
                 st.session_state.step1 = True
-
         except Exception as e:
             st.error("❌ Failed to contact Agent.")
             st.exception({e})
-
-#Step 2: Inquirer sends K+(IRN)+RN[k] to Agent
-if st.session_state.step1 and not st.session_state.step2:
-    if st.button("▶️ Step 2"):
-        #
-        #st.session_state.IRN = random.randint(10000, 99999)
-        #st.session_state.encrypted_irn = pow(st.session_state.IRN, st.session_state.public_key, st.session_state.modulus)
-        #st.session_state.step2_value = st.session_state.encrypted_irn + st.session_state.RN[st.session_state.k]
-        st.session_state.step2 = True
-     st.write(f"IRN = {st.session_state.IRN}")
-    st.write(f"Encrypted IRN = {st.session_state.encrypted_irn}")
-    st.write(f"Sent to Agent (step2_value) = {st.session_state.step2_value}")      
-            try:
-                response = requests.post(f"{agent_url}/step3", json={
-                    "step2_value": str(st.session_state.step2_value)
-                }).json()
-
-                st.session_state.responses = response["responses"]
-                st.session_state.final_values = [
-                    r - st.session_state.IRN for r in st.session_state.responses
-                ]
-                st.session_state.step2_done = True
-            except Exception as e:
-                st.error(f"❌ Failed to send step2_value: {e}")
